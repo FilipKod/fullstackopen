9658503e590afe5e8afa14ac2dbe84ed89ad8544
Filip Madunicky <yelidric@gmail.com>
2025-09-15 11:44:54 +0200

    Adds interactive note filtering
    Introduces functionality to add new notes, toggle their importance,
    and filter notes based on importance. This improves the app's
    interactivity and allows users to manage notes more effectively.

--- renderdemo/src/App.jsx
+++ renderdemo/src/App.jsx
@@ -1,16 +1,50 @@
+import { useState } from "react";
 import Note from "./components/Note";
 
-function App({ notes }) {
+const App = ({ notes }) => {
+  const [appNotes, setAppNotes] = useState(notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObj = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(appNotes.length + 1),
+    };
+
+    setAppNotes(appNotes.concat(noteObj));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? appNotes
+    : appNotes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
