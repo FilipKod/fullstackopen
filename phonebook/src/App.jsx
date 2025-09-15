4adcdff24c8cec792c8de8fcdd440c3c101b6893
Filip Madunicky <yelidric@gmail.com>
2025-09-15 16:37:27 +0200

    Adds functionality to add new persons
    Implements the functionality to add new persons to the phonebook.
    The form now handles user input, creates a new person object,
    and updates the list of persons. The input field is cleared
    after submission. Also displays the list of added names.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -4,18 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPersonHandler = (event) => {
+    event.preventDefault();
+    const personObj = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObj));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPersonHandler}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={person.name + index}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
