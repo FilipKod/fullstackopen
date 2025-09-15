ada70cead6419de4f8f0032dca9bc7142de2bcad
Filip Madunicky <yelidric@gmail.com>
2025-09-15 17:10:28 +0200

    Adds number input to phonebook entries
    Extends phonebook entries to include phone numbers.
    This enhancement allows users to store and display
    contact numbers alongside names. It also prevents
    duplicate entries based on name and number.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,28 +1,40 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "0923432432" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addPersonHandler = (event) => {
     event.preventDefault();
 
-    console.log(newName);
-
     if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook.`);
+      alert(`Name ${newName} is already added to phonebook.`);
+      return;
+    }
+
+    if (persons.some((person) => person.number === newNumber)) {
+      alert(`Number ${newNumber} is already added to phonebook.`);
       return;
     }
 
     const personObj = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(personObj));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -33,13 +45,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
+          number <input value={newNumber} onChange={handleNewNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
-          <li key={person.name + index}>{person.name}</li>
+        {persons.map((person) => (
+          <li key={person.name + person.number}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
     </div>
