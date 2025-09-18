58f70f278309db004feecac80c6433a4ea738c02
Filip Madunicky <yelidric@gmail.com>
2025-09-18 18:09:47 +0200

    Adds JSON server for data fetching
    Adds json-server dependency and configures scripts to run a mock backend.
    This enables fetching data for the phonebook and renderdemo applications,
    facilitating data persistance and separation from the front end.

--- renderdemo/src/App.jsx
+++ renderdemo/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
 
-const App = ({ notes }) => {
-  const [appNotes, setAppNotes] = useState(notes);
+const App = () => {
+  const [appNotes, setAppNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fullfiled");
+      setAppNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  console.log("render", appNotes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
