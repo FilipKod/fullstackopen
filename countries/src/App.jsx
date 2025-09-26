6de218876c81e643d4f0291321dcd2ae80bcf594
Filip Madunicky <yelidric@gmail.com>
2025-09-27 00:20:34 +0200

    Displays country information based on filter
    Implements the logic to display country data
    based on the search filter.
    
    If more than 10 matches are found, a warning message is shown.
    If between 2 and 10 matches are found, a list of country names is shown.
    If only one match is found, detailed information about the country is displayed,
    including capital, area, languages, and flag.
    If the input is empty, nothing is displayed.

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -19,11 +19,48 @@ function App() {
   };
 
   const filteredCountries = () => {
-    console.log(
-      countries.filter((country) =>
-        country.name.common.toLowerCase().includes(value)
-      )
+    const filteredData = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(value)
     );
+
+    if (!value) return null;
+
+    if (filteredData.length > 10) {
+      return <p>Too many matches, specify another filter</p>;
+    }
+
+    if (filteredData.length > 1 && filteredData.length <= 10) {
+      return (
+        <ul>
+          {filteredData.map((el) => (
+            <li key={String(el.idd.root + el.idd.suffixes.join(""))}>
+              {el.name.common}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+
+    if (filteredData.length === 1) {
+      const country = filteredData[0];
+
+      // console.log(country);
+      // country.flags.png
+      return (
+        <div>
+          <h1>{country.name.common}</h1>
+          <div>Capital {country.capital}</div>
+          <div>Area {country.area}</div>
+          <h2>Languages</h2>
+          <ul>
+            {Object.entries(country.languages).map(([key, value]) => (
+              <li key={key}>{value}</li>
+            ))}
+          </ul>
+          <img src={country.flags.png} />
+        </div>
+      );
+    }
   };
 
   return (
